57f16fae98efa2b33273afd4653314f41be5aac3
2025da04151-stack <2025da04151@wilp.bits-pilani.ac.in>
2026-08-01 21:48:35 +0530

    Refactor model prediction variables in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -158,22 +158,22 @@ with tab_all_compare:
     #eval_tab += "|-------|----------|-----|-----------|--------|-----|-----|\n"
     for classification_model in MODEL_DUMP_FILES.keys():
         cmodel = load_classification_model(classification_model)
-        y_pred[classification_model] = cmodel.predict(X_test)
-        y_proba[classification_model] = cmodel.predict_proba(X_test)[:, 1]
+        y_pred_model[classification_model] = cmodel.predict(X_test)
+        y_proba_model[classification_model] = cmodel.predict_proba(X_test)[:, 1]
         #eval_tab += f"| {classification_model} | {accuracy_score(y, y_pred):.4f} | {roc_auc_score(y, y_proba):.4f} | {precision_score(y, y_pred):.4f} | {recall_score(y, y_pred):.4f} | {f1_score(y, y_pred):.4f} | {matthews_corrcoef(y, y_pred):.4f} |\n"
     #st.markdown(eval_tab)
     def eval_metric(model):
-        st.metric("Accuracy", f"{accuracy_score(y, y_pred[model]):.4f}")
+        st.metric("Accuracy", f"{accuracy_score(y, y_pred_model[model]):.4f}")
         st.divider()
-        st.metric("AUC Score", f"{roc_auc_score(y, y_proba[model]):.4f}")
+        st.metric("AUC Score", f"{roc_auc_score(y, y_proba_model[model]):.4f}")
         st.divider()
-        st.metric("Precision", f"{precision_score(y, y_pred[model]):.4f}")
+        st.metric("Precision", f"{precision_score(y, y_pred_model[model]):.4f}")
         st.divider()
-        st.metric("Recall", f"{recall_score(y, y_pred[model]):.4f}")
+        st.metric("Recall", f"{recall_score(y, y_pred_model[model]):.4f}")
         st.divider()
-        st.metric("F1 Score", f"{f1_score(y, y_pred[model]):.4f}")
+        st.metric("F1 Score", f"{f1_score(y, y_pred_model[model]):.4f}")
         st.divider()
-        st.metric("MCC", f"{matthews_corrcoef(y, y_pred[model]):.4f}")
+        st.metric("MCC", f"{matthews_corrcoef(y, y_pred_model[model]):.4f}")
         st.divider()
     with col_lr:
         eval_metric("Logistic Regression")
