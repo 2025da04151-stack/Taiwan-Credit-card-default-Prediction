79e0869d059e4b6b1694ba27db7bf247ee9f552a
2025da04151-stack <2025da04151@wilp.bits-pilani.ac.in>
2026-08-05 00:49:58 +0530

    Rename download button label for clarity

--- streamlit_app.py
+++ streamlit_app.py
@@ -166,7 +166,7 @@ with tab_selected_model:
     # Download button for downloading prediction data
     df_test_download_csv=df_test_download.to_csv(index=False).encode("utf-8")
     st.download_button(
-    label="Download Prediction CSV",
+    label="Download Full Prediction CSV",
     data=df_test_download_csv,
     file_name="Credit_Card_Prediction.csv",
     mime="text/csv",
